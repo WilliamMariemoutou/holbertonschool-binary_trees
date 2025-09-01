2e01db8deaf9d4b994fb82c7b696cf1616e9bf68
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-09-01 20:53:36 +0400

    Goes through a binary tree using inorder traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -12,16 +12,11 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
+	if (tree == NULL || func == NULL)
 		return;
 
-	if (func == NULL)
-		return;
-
-	func = tree;
 	binary_tree_inorder(tree->left, func);
+	func(tree->n)
 	binary_tree_inorder(tree->right, func);
-	binary_tree_inorder(tree->left->left, func);
-	binary_tree_inorder(tree->right->right, func);
 
 }
