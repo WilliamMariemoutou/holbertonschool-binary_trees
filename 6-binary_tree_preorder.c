e5c3b792893c8f2d2718021fd6fbaedae482d6ed
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-09-01 14:01:03 +0400

    Goes through a binary tree using preorder tansversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -18,6 +18,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	func(tree->n);
-	tree->left;
-	tree->right;
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
